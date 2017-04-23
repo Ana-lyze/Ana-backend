311fed8232def4010b437253ebe3e5dbbaa807e5
Jason Park <parkjs814@gmail.com>
2017-04-23 00:45:53 -0500

    Kms

--- index.js
+++ index.js
@@ -4,16 +4,17 @@ const async = require('async');
 const ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
 const app = express();
 const bodyParser = require('body-parser');
+const snoowrap = require('snoowrap');
 
 const secret = require('./secret');
 
-const client = new Twitter(secret.twitter);
+const twitter = new Twitter(secret.twitter);
+const reddit = new snoowrap(secret.reddit);
 const tone_analyzer = new ToneAnalyzerV3(secret.watson);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) => {
-  console.log(req.body);
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type');
@@ -25,11 +26,11 @@ app.options('*', (req, res, next) => {
   res.send();
 });
 
-const search = (q, result_type, cb) => {
-  client.get(`search/tweets.json?q=${q}&result_type=${result_type}&tweet_mode=extended`, (err, { statuses }) => {
+const twitterSearch = (q, result_type, cb) => {
+  twitter.get(`search/tweets.json?q=${q}&result_type=${result_type}&tweet_mode=extended`, (err, { statuses }) => {
     if (err) return cb(err);
     if (statuses.length) return cb(null, statuses);
-    if (result_type == 'popular') return search(q, 'recent', cb);
+    if (result_type == 'popular') return twitterSearch(q, 'recent', cb);
     cb(new Error('No Data'));
   });
 };
@@ -38,10 +39,19 @@ app.get('/', (req, res, next) => {
   async.waterfall([
     (cb) => {
       const { q } = req.query;
-      search(q, 'popular', cb);
+      async.parallel({
+        twitter: (cb) => {
+          twitterSearch(q, 'popular', cb);
+        },
+        reddit: (cb) => {
+          reddit.search({ query: q })
+            .then(cb.bind(this, null))
+            .catch(cb);
+        },
+      }, cb);
     },
-    (statuses, cb) => {
-      res.json(statuses);
+    ({ twitter, reddit }, cb) => {
+      res.json({ twitter, reddit });
     }
   ], next);
 });
